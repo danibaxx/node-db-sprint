f04884736ad83af7284f6bd51077f740de67f1ec
Danielle B <danibaxx@hotmail.com>
2020-01-19 17:28:38 -0700

    debugging migration issue, Error: SQLITE_ERROR: near foreign: syntax error

--- data/migrations/20200119170023_initial.js
+++ data/migrations/20200119170023_initial.js
@@ -8,24 +8,23 @@ exports.up = async function(knex) {
 
   await knex.schema.createTable("resources", (table) => {
     table.increments("resource_id")
-    table.string("resource_name", 128).notNullable().unique()
-    table.string("resource_description", 255)
+    table.string("resource_name", 128).notNullable()
+    table.string("resource_description", 255).notNullable()
   });
 
   await knex.schema.createTable("tasks", (table) => {
     table.increments("task_id")
-    table.string("task_description", 128)
-    table.string("task_notes", 255)
+    table.string("task_description", 128).notNullable()
+    table.string("task_notes", 255).notNullable()
     table.boolean("task_completed").defaultTo(0).notNullable()
     table
     .integer("project_id")
     .notNullable()
-    .references("id")
+    .references("project_id")
     .inTable("projects")
   });
 
-  await knex.schema.createTable("projects_resources", (table) => {
-    table.increments("project_resouce_id")
+  await knex.schema.createTable("project_resources", (table) => {
     table.integer("project_id")
       .notNullable()
       .references("project_id")
@@ -34,11 +33,12 @@ exports.up = async function(knex) {
       .notNullable()
       .references("resource_id")
       .inTable("resources")
+    table.primary(["project_id", "resource_id"])
   });
 };
 
 exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists("projects_resources")
+  await knex.schema.dropTableIfExists("project_resources")
   await knex.schema.dropTableIfExists("task")
   await knex.schema.dropTableIfExists("resources")
   await knex.schema.dropTableIfExists("projects")
