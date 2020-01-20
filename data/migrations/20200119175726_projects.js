2d0b7b2a00a21c8098e559a9d545ba8959964734
Danielle B <danibaxx@hotmail.com>
2020-01-19 18:06:10 -0700

    having issues with migrate:latest, thinking its because I need to make my seeds first

--- data/migrations/20200119175726_projects.js
+++ data/migrations/20200119175726_projects.js
@@ -1,3 +1,4 @@
+
 exports.up = async function(knex) {
   await knex.schema.createTable("projects", (table) => {
     table.increments("project_id")
@@ -8,23 +9,22 @@ exports.up = async function(knex) {
 
   await knex.schema.createTable("resources", (table) => {
     table.increments("resource_id")
-    table.string("resource_name", 128).notNullable()
+    table.string("resource_name", 128).notNullable().unique()
     table.string("resource_description", 255).notNullable()
   });
 
   await knex.schema.createTable("tasks", (table) => {
     table.increments("task_id")
-    table.string("task_description", 128).notNullable()
+    table.string("task_description", 255).notNullable()
     table.string("task_notes", 255).notNullable()
     table.boolean("task_completed").defaultTo(0).notNullable()
-    table
-    .integer("project_id")
-    .notNullable()
-    .references("project_id")
-    .inTable("projects")
+    table.integer("project_id")
+      .notNullable()
+      .references("project_id")
+      .inTable("projects")
   });
 
-  await knex.schema.createTable("project_resources", (table) => {
+  await knex.schema.createTable("projects_resources", (table) => {
     table.integer("project_id")
       .notNullable()
       .references("project_id")
@@ -38,8 +38,8 @@ exports.up = async function(knex) {
 };
 
 exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists("project_resources")
-  await knex.schema.dropTableIfExists("task")
+  await knex.schema.dropTableIfExists("projects_resources")
+  await knex.schema.dropTableIfExists("tasks")
   await knex.schema.dropTableIfExists("resources")
   await knex.schema.dropTableIfExists("projects")
 };
